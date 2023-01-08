3f8c8917b10e3c55589319ca738c4c0c70e751d1
Malte Mosbach <maltemosbach@gmail.com>
2023-01-08 17:40:49 +0100

    Initial personal page

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+let papertitle = "Malte Mosbach"
+
+// set papertitle
+document.getElementById("papertitle_website").innerHTML = papertitle;
+document.getElementById("papertitle_heading").innerHTML = papertitle;
