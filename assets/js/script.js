276451407bbd1d0c0872920c9013aeadb7d599dc
Malte Mosbach <maltemosbach@gmail.com>
2024-06-26 15:04:26 +0200

    Update website.

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-let papertitle = "Malte Mosbach"
-
-// set papertitle
-document.getElementById("papertitle_website").innerHTML = papertitle;
-document.getElementById("papertitle_heading").innerHTML = papertitle;
